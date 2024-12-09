48469a7b72ed3d4b387d006f87e635650e65610a
messense <messense@icloud.com>
2024-12-09 21:47:35 +0800

    Manage zsh config with home-manager

--- home/default.nix
+++ home/default.nix
@@ -1,6 +1,9 @@
 { username, ... }:
 
 {
+  imports = [
+    ./shell.nix
+  ];
   # Home Manager needs a bit of information about you and the
   # paths it should manage.
   home = {
---
+++ home/shell.nix
@@ -0,0 +1,48 @@
+{ ... }:
+{
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+    antidote = {
+      enable = true;
+      plugins = [
+        "zsh-users/zsh-completions"
+        "getantidote/use-omz"
+        "ohmyzsh/ohmyzsh path:lib"
+      ];
+    };
+    initExtra = ''
+      export PATH="$HOME/.local/bin:$HOME/.cargo/bin:/opt/homebrew/bin:$PATH"
+
+      # Windsurf
+      export PATH="$HOME/.codeium/windsurf/bin:$PATH"
+
+      # LM Studio CLI (lms)
+      export PATH="$PATH:$HOME/.cache/lm-studio/bin"
+    '';
+    envExtra = ''
+      export HOMEBREW_BAT=1
+      export BAT_THEME="Sublime Snazzy"
+    '';
+  };
+
+  programs.direnv = {
+    enable = true;
+    enableZshIntegration = true;
+  };
+
+  programs.starship = {
+    enable = true;
+    enableZshIntegration = true;
+  };
+
+  programs.fzf = {
+    enable = true;
+    enableZshIntegration = true;
+  };
+
+  programs.zoxide = {
+    enable = true;
+    enableZshIntegration = true;
+  };
+}
