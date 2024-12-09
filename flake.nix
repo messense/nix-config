dc1b2d46873093070f42d718765486a78676fc35
messense <messense@icloud.com>
2024-12-09 19:32:48 +0800

    Add nixfmt

--- flake.nix
+++ flake.nix
@@ -11,6 +11,7 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    treefmt-nix.url = "github:numtide/treefmt-nix";
   };
 
   outputs =
@@ -18,7 +19,9 @@
       self,
       nix-darwin,
       nixpkgs,
+      systems,
       home-manager,
+      treefmt-nix,
       ...
     }:
     let
@@ -26,8 +29,21 @@
       specialArgs = inputs // {
         inherit username;
       };
+
+      # Small tool to iterate over each systems
+      eachSystem = f: nixpkgs.lib.genAttrs (import systems) (system: f nixpkgs.legacyPackages.${system});
+
+      # Eval the treefmt modules from ./treefmt.nix
+      treefmtConfig = import ./treefmt.nix;
     in
     {
+      # for `nix fmt`
+      formatter = eachSystem (pkgs: treefmt-nix.lib.mkWrapper pkgs treefmtConfig);
+      # for `nix flake check`
+      checks = eachSystem (pkgs: {
+        formatting = (treefmt-nix.lib.evalModule pkgs treefmtConfig).config.build.check self;
+      });
+
       darwinConfigurations."mac-mini" = nix-darwin.lib.darwinSystem {
         inherit specialArgs;
         modules = [
