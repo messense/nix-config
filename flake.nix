54a3d2daff12c52f6a6052b0f7451c79dcc2e53e
messense <messense@icloud.com>
2024-12-09 19:18:08 +0800

    Setup home-manager

--- flake.nix
+++ flake.nix
@@ -7,9 +7,19 @@
       url = "github:LnL7/nix-darwin";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs }:
+  outputs = inputs@{
+    self,
+    nix-darwin,
+    nixpkgs,
+    home-manager,
+    ...
+  }:
   let
     username = "messense";
     specialArgs =
@@ -23,6 +33,14 @@
       inherit specialArgs;
       modules = [
         ./hosts/mac-mini
+
+        home-manager.darwinModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.extraSpecialArgs = specialArgs;
+          home-manager.users.${username} = import ./home;
+        }
       ];
     };
   };
